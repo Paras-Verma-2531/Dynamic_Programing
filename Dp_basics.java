7f6338315860402d501831fb4cd15b8cac8ac72b
paras <parasverma2531@gmail.com>
2023-02-03 23:44:40 +0530

    Steps for recurrence relation

---
+++ Dp_basics.java
@@ -0,0 +1,8 @@
+public class Dp_basics {
+    /*
+    Steps for creating recurrence relations:
+    1): try to represent the problem in terms of index
+    2): do all possible stuffs acc to problem statement
+    3): sum of all the stuffs -> return min/max
+     */
+}
